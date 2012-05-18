18d0b5066770560ccb74008b7b53c1a62e94e4c0
Matthew King <mking@borderstylo.com>
2012-05-18 12:20:06 -0700

    yardoc for V0

--- lib/desire/v0/composites.rb
+++ lib/desire/v0/composites.rb
@@ -1,3 +1,9 @@
+class Desire
+  # Namespace for versioning the conventions used by wrapper classes.
+  module V0
+  end
+end
+
 require "desire/v0/sorted_hash"
 require "desire/v0/collector"
 require "desire/v0/time_slicer"
