88476d739111160c52036d6349f4ebe06ac250a0
Matthew King <mking@borderstylo.com>
2012-05-21 16:22:14 -0700

    init pages

--- lib/desire/composite.rb
+++
@@ -1,9 +0,0 @@
-class Desire
-
-  # Mixin providing common methods and attributes for composite wrapper classes
-  module Composite
-
-    attr_reader :client, :base_key
-  end
-
-end
--- test/setup.rb
+++
@@ -1,9 +0,0 @@
-require "rubygems"
-require "mock_redis"
-
-here = File.dirname(__FILE__)
-DESIRE_ROOT = File.expand_path("#{here}/..")
-$LOAD_PATH.unshift("#{DESIRE_ROOT}/lib")
-
-require "desire"
-
